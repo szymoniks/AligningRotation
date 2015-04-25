aa6a4b8e4091650c591587923dd28d84f101f279
Michael Detmold <mdetmold@gmail.com>
2015-04-25 17:05:00 -0500

    contouring, centering, etc.. on the way

--- contourise.py
+++ contourise.py
@@ -1,5 +1,5 @@
 
-import cv2
+import cv2, itertools, glob
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -100,7 +100,7 @@ def match(img1,img2):
     #plt.imshow(img3),plt.show()
 def orb(img):
     #img = cv2.imread(img_pth,0)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Initiate STAR detector
     orb = cv2.ORB()
@@ -115,15 +115,84 @@ def orb(img):
     img2 = cv2.drawKeypoints(img,kp[:20],color=(0,255,0), flags=0)
     plt.imshow(img2),plt.show()
 
+def get_cnt(image):
+    #imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    height, width = image.shape[:2]
+    ret,thresh = cv2.threshold(image,127,255,0)
+    _, contours0, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours = np.array([cv2.approxPolyDP(cnt, 3, True) for cnt in contours0])
+    return contours
+def return_empty_image(h,w):
+    empty = np.zeros((h,w,3), np.uint8)
+    empty[::] = (255,255,255)
+    return empty
+def apply_contour(cnt, image):   
+    cv2.drawContours(image,cnt,-1,(0,255,0),3)
+    return empty
+
 def contourise(image):
     imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     height, width, depth = img.shape
     empty = np.zeros((height,width,3), np.uint8)
     empty[::] = (255,255,255)    
     ret,thresh = cv2.threshold(imgray,127,255,0)
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    _, contours0, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours = np.array([cv2.approxPolyDP(cnt, 3, True) for cnt in contours0])
+    print(len(contours))
     cv2.drawContours(empty,contours,-1,(0,255,0),3)
-    return empty
+
+    return empty, contours
+
+
+def translate_img(img, x, y):
+    M = np.matrix([[1,0,x],[0,1,y]])
+    cv2.warpAffine(img, M, img.shape[:2]) 
+    return img
+def translate_cnt(cnt, x, y):
+    for poly in cnt:
+        for point in poly:
+            point[0][0] += x
+            point[0][1] += y
+    return cnt
+def center_cnt(cnt, img):
+    cnt_rect = np.array([ item for l in cnt for item in l]) 
+    #print "cnts: {}".format(cnt)
+    imh,imw = img.shape[:2]
+    x,y,w,h = cv2.boundingRect(cnt_rect)
+    shiftx = (imw*.5) - (x + (.5*w))
+    shifty = (imh*.5) - (y + (.4*h))
+    cnt = translate_cnt(cnt, shiftx, shifty)
+    return cnt
+#def center(img, cnt):
+#    cnt = np.array([ item for l in cnt for item in l]) 
+#    print "cnts: {}".format(cnt)
+#    imh,imw = img.shape[:2]
+#    x,y,w,h = cv2.boundingRect(cnt)
+#    shiftx = (imw*.5) - (x + (.5*w))
+#    shifty = (imh*.5) - (y + (.4*h))
+#    img = translate_img(img, shiftx, shifty)
+#    return img
+def draw_bound(img, cnt):
+    x,y,w,h = cv2.boundingRect(cnt)
+    rect = ((x,y),(x+w,y+h))
+    cv2.rectangle(img, rect[0], rect[1], (0,0,0))
+    return img
+def correct(img, cnt):
+    
+    height, width, _ = img.shape
+    print "h {} w {}".format(height, width)
+# x,y = width, height
+    punto = (int(width*.85), int(height*.25))
+    pt_cof = (.85,.1)
+    
+    checkpoints = itertools.product(pt_cof, repeat=2)
+    for coef in checkpoints:
+        pt = (int(width*coef[0]), int(height*coef[1]))
+        dist = min([cv2.pointPolygonTest(c, pt,True) for c in cnt])
+        cv2.putText(img, str(dist), pt, cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+        cv2.circle(img, pt, 10, (255, 0, 0))  
+    #img = draw_bound(img, cnt)
+    return img
 #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 #kernel = np.ones((5,5),np.uint8)
@@ -134,19 +203,31 @@ def contourise(image):
 
 #plt.imshow(contours,cmap = 'gray')
 #plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    # directory = argv[0]
+    # os.chdir(directory)
+    # for file in glob.glob("*.obj"):
+    #    print("modifying mesh " + file)
+    #    scaleAndMove.main([file, os.getcwd()])
+testers = glob.glob("*.png")
 impath = 'test/0x0.png' #master
+#impath = testers[2]
 impath2 = '0.25x1.0.png'
 img = cv2.imread(impath) 
 img2 = cv2.imread(impath2) 
-#img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#
 #invert the images
 cv2.bitwise_not(img, img)
-cv2.bitwise_not(img2, img2)
-
-contoured1 = contourise(img)
-show(contoured1)
+#cv2.bitwise_not(img2, img2)
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+height, width = img.shape[:2]
+cnt = get_cnt(img)
+empty = return_empty_image(height,width)
+cnt = center_cnt(cnt, empty)
+empty = apply_contour(cnt, empty)
 #contoured2 = contourise(img2)
 #contoured1 = cv2.cvtColor(contoured1,cv2.COLOR_BGR2GRAY)
+empty = correct(empty,cnt)
+show(empty)
 #contoured2 = cv2.cvtColor(contoured2,cv2.COLOR_BGR2GRAY)
 #match(contoured1, contoured2)
 #orb(contoured)
