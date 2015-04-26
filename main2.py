b3548f50b1ad74f008faa41af3fd23a8ff282f4b
Michael Detmold <mdetmold@gmail.com>
2015-04-26 13:19:28 -0500

    added a simplified main file

--- main2.py
+++ main2.py
@@ -2,12 +2,6 @@ import sys, glob, os
 import scaleAndMove
 import math
 import subprocess
-
-def drange(start, stop, step):
-    r = start
-    while r < stop:
-        yield r
-        r += step
         
 def run_command(cmd):
     print "\n\n" + cmd
@@ -24,7 +18,6 @@ def main(argv):
     scaleAndMove.main([fileName, scaledMeshFileName])
     
     pi = math.pi
-    steps = int(math.ceil((2*pi)/samplesPerR))
     for x in range(samplesPerR+1)[1:]:
     
         theta = (2*pi) * (float(x)/samplesPerR)
@@ -34,12 +27,6 @@ def main(argv):
             phi = (2*pi) * (float(y)/samplesPerR)
             run_command("blender -b -P screenshot.py -- " + scaledMeshFileName + " " + str(theta) + " " + str(phi) + " 30 " + outFolder + str(theta) + "x" + str(phi))
 
-#    for x in drange(.001, 1.00001, 1.0/(float(samplesPerR)-1)):
-#        theta = 2 * math.pi * x - math.pi
-#        for y in drange(0, 1.00001, 1.0/(float(samplesPerR)-1)):
-#            phi = math.acos(2*y - 1.0) - math.pi
-#            run_command("blender -b -P screenshot.py -- " + scaledMeshFileName + " " + str(theta) + " " + str(phi) + " 30 " + outFolder + str(x) + "x" + str(y))
-            
             
 
 if __name__ == "__main__":
